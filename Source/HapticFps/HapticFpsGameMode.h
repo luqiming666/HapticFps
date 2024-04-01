2e1928a1a3ef866449b059e1b30103649a1831d7
Qiming <LuQiMing@aactechnologies.com>
2024-04-01 13:51:52 +0800

    add force feedback support

---
+++ Source/HapticFps/HapticFpsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HapticFpsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHapticFpsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHapticFpsGameMode();
+};
+
+
+
